caff8a9fec80b32d1ed590fc4fa4ab2249a55b7c
David Lopez <108307450+DLopez03@users.noreply.github.com>
2024-04-29 20:39:25 -0700

    Update muxes.sv

--- muxes.sv
+++ muxes.sv
@@ -1,7 +1,7 @@
 module muxes (
     input logic a_i, b_i, c_i, d_i,
     input logic [1:0] sel4_i,   
-    output logic y0_o, y1_o     
+    output logic y0, y1     
 );
 
     logic xor_out;  
@@ -11,20 +11,20 @@ module muxes (
 
     always_comb begin
         if (xor_out) begin
-            y0_o = d_i;
+            y0 = d_i;
         end else begin
-            y0_o = c_i;
+            y0 = c_i;
         end
     end
 
     
     always_comb begin
         case (sel4_i)
-            2'b00: y1_o = 0;   
-            2'b01: y1_o = c_i; 
-            2'b10: y1_o = 0;   
-            2'b11: y1_o = d_i; 
-            default: y1_o = 0; 
+            2'b00: y1 = 0;   
+            2'b01: y1 = c_i; 
+            2'b10: y1 = 0;   
+            2'b11: y1 = d_i; 
+            default: y1 = 0; 
         endcase
     end
 
